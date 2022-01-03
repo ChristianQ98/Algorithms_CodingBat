050f2c4e9bffa3764d52a468bb511df25626ad01
ChristianQ98 <christian.quintero098@gmail.com>
2022-01-03 11:41:39 -0800

    Initial commit

---
+++ Java/WarmUp-1/diff21/Test.java
@@ -0,0 +1,13 @@
+package diff21;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution test1 = new Solution();
+        Solution test2 = new Solution();
+        Solution test3 = new Solution();
+
+        System.out.println(test1.diff21(19));
+        System.out.println(test2.diff21(10));
+        System.out.println(test3.diff21(21));
+    }
+}
