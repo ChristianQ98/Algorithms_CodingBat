a153052c213be346dc513f00ad23c09c0fd750a8
ChristianQ98 <christian.quintero098@gmail.com>
2022-01-04 16:10:53 -0800

    Initial commit

---
+++ Java/WarmUp-1/nearHundred/Test.java
@@ -0,0 +1,13 @@
+package nearHundred;
+
+public class Test {
+    public static void main(String[] args) {
+    Solution test1 = new Solution();
+    Solution test2 = new Solution();
+    Solution test3 = new Solution();
+
+    System.out.println(test1.nearHundred(93));
+    System.out.println(test2.nearHundred(90));
+    System.out.println(test3.nearHundred(89));
+    }
+}
