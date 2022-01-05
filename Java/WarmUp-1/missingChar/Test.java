10f54d19545e6b6602f856ca48a316fa7568bbdb
ChristianQ98 <christian.quintero098@gmail.com>
2022-01-05 15:03:36 -0800

    Initial commit

---
+++ Java/WarmUp-1/missingChar/Test.java
@@ -0,0 +1,13 @@
+package missingChar;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution test1 = new Solution();
+        Solution test2 = new Solution();
+        Solution test3 = new Solution();
+
+        System.out.println(test1.missingChar("kitten", 1));
+        System.out.println(test2.missingChar("kitten", 0));
+        System.out.println(test3.missingChar("kitten", 4));
+    }
+}
