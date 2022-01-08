df74b86809b65a7865f76b764c7b5e33d761d66b
ChristianQ98 <christian.quintero098@gmail.com>
2022-01-07 21:42:46 -0800

    Initial commit

---
+++ Java/WarmUp-1/backAround/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+
+    /*
+    
+    Given a string, take the last char and return a new string with the last char added at the front and back, 
+    so "cat" yields "tcatt". The original string will be length 1 or more.
+
+    backAround("cat") → "tcatt"
+    backAround("Hello") → "oHelloo"
+    backAround("a") → "aaa"
+    
+    */
+
+    public String backAround(String str) {
+        if(str.isEmpty()) {
+            return "";
+        } else {
+            String last = str.substring(str.length() - 1);
+            return last.concat(str).concat(last);
+        }
+    }
+}
---
+++ Java/WarmUp-1/backAround/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        Solution test1 = new Solution();
+        Solution test2 = new Solution();
+        Solution test3 = new Solution();
+
+        System.out.println(test1.backAround("cat"));
+        System.out.println(test2.backAround("Hello"));
+        System.out.println(test3.backAround("a"));
+    }
+}
